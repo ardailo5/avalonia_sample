632b17ba6ccd3f324b04d2df4335dbc590e8e901
ardailo5 <apollosrazodailo5@gmail.com>
2026-09-08 22:11:16 +0800

    Added new window and login button now goes to that new window if username and password is correct

---
+++ test/Dash.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace test;
+
+public partial class Dash : Window
+{
+    public Dash()
+    {
+        InitializeComponent();
+    }
+}
--- test/MainWindow.axaml.cs
+++ test/MainWindow.axaml.cs
@@ -12,17 +12,18 @@ public partial class MainWindow : Window
 
     public void OnButtonClick(object? sender, RoutedEventArgs e)
     {
+        var dash = new Dash();
         string username = Tb_Uname.Text ?? string.Empty;
         string password = Tb_Pword.Text ?? string.Empty;
-
+        
         if (username == "admin" && password == "admin")
         {
-            Lbl_Uname.Text = $"Username {username} is correct!";
-            Lbl_Pword.Text = $"Password {password} is correct!";
+           dash.Show();
+           this.Close();
         }
         else
         {
-            Lbl_Uname.Text = $"Incorrect username or password!";
+            Lbl_ErrorMsg.Text = $"Incorrect username or password!";
         }
 
     }
