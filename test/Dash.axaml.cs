93e8c1b7c14658728e4b3ab3e6611a2d82ae51b1
apollos <apollosrazodailo5@gmail.com>
2026-09-12 11:11:27 +0800

    Added a window and a usercontrol

--- test/Dash.axaml.cs
+++ test/Dash.axaml.cs
@@ -10,7 +10,7 @@ public partial class Dash : Window
     public Dash()
     {
         InitializeComponent();
-        container.Children.Add(new UserControl1());
+        container.Children.Add(new UserControl4());
     }
 
     public void FirstPanel(object sender, RoutedEventArgs e)
--- test/obj/Debug/net10.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59615859170f0f1c1482aab2bf2a669a31e88b8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0485a6d3d3b334406360ba12738ff03704d4c524")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
